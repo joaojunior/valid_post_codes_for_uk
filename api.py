0b8da5be1e1fd3c9dc7875b60f3a34d2235e03e3
Joao Junior <jcajcefet@yahoo.com.br>
2017-04-10 21:33:48 +0200

    Add flask in requirements and blank spaces extras in parameter for api

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
-from flask import Flask
 from flask import abort
+from flask import Flask
 
 from validate_post_code.post_code import PostCodeUK
 
@@ -8,9 +8,14 @@ post_code_validator = PostCodeUK()
 
 
 @app.route('/validate/<post_code>')
-def show_user_profile(post_code):
+def validate_post_code(post_code):
+    post_code = post_code.strip()
     result = post_code_validator.validate_post_code_for_uk(post_code)
     if result:
         return 'The Post code: {0} is valid!'.format(post_code)
     else:
         abort(422, {'errors': dict(post_code="This post code isn't valid")})
+
+
+if __name__ == "__main__":
+    app.run()
