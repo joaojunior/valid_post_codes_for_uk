ef51d5569f5d390589dfb265b7a8263cad5b3930
Joao Junior <jcajcefet@yahoo.com.br>
2017-04-10 16:33:12 +0200

    Refactor the code to create a class and clean the code

--- post_code.py
+++ post_code.py
@@ -1,86 +1,84 @@
-def validate_post_code_for_uk(post_code):
-    return (_valid_size(post_code) and
-            _valid_space_position(post_code) and
-            _valid_last_3_positions(post_code) and
-            _verify_first_position(post_code) and
-            _verify_second_position(post_code) and
-            _verify_third_position(post_code) and
-            _verify_fourth_position(post_code))
+class PostCodeUK():
+    def validate_post_code_for_uk(self, post_code):
+        self.post_code = post_code
+        return (self._valid_size() and
+                self._valid_space_position() and
+                self._valid_last_3_positions() and
+                self._verify_first_position() and
+                self._verify_second_position() and
+                self._verify_third_position() and
+                self._verify_fourth_position())
 
+    def _valid_size(self):
+        result = True
+        size = len(self.post_code)
+        if size < 6 or size > 8:
+            result = False
+        return result
 
-def _valid_size(post_code):
-    result = True
-    size = len(post_code)
-    if size < 6 or size > 8:
-        result = False
-    return result
+    def _valid_space_position(self):
+        size = len(self.post_code)
+        result = True
+        if self.post_code[size - 4] != ' ':
+            result = False
+        return result
 
+    def _valid_last_3_positions(self):
+        """
+        The last 3 positions have the format: 9AA, where
+        9 represent one digit and A represent one letter.
+        The final two letters do not use the letters CIKMOV
+        """
+        not_allowed = ['C', 'I', 'K', 'M', 'O', 'V']
+        post_code = self.post_code
+        result = (post_code[-1].isalpha() and
+                  post_code[-1] not in not_allowed and
+                  post_code[-2].isalpha() and
+                  post_code[-2] not in not_allowed and
+                  post_code[-3].isdigit())
+        return result
 
-def _valid_space_position(post_code):
-    size = len(post_code)
-    result = True
-    if post_code[size - 4] != ' ':
-        result = False
-    return result
+    def _verify_first_position(self):
+        """
+        The first position is not a number
+        The letters QVX are not used in the first position
+        """
+        not_allowed = ('Q', 'V', 'X')
+        result = self.post_code[0].isalpha()
+        if self.post_code[0] in not_allowed:
+            result = False
+        return result
 
+    def _verify_second_position(self):
+        """
+        The second position is a number or a letter
+        The letters IJZ are not used in the second position
+        """
+        not_allowed = ('I', 'J', 'Z')
+        result = self.post_code[1].isalpha() or self.post_code[1].isdigit()
+        if self.post_code[1] in not_allowed:
+            result = False
+        return result
 
-def _valid_last_3_positions(post_code):
-    """
-    The last 3 positions have the format: 9AA, where
-    9 represent one digit and A represent one letter.
-    The final two letters do not use the letters CIKMOV
-    """
-    not_allowed = ['C', 'I', 'K', 'M', 'O', 'V']
-    result = (post_code[-1].isalpha() and post_code[-1] not in not_allowed and
-              post_code[-2].isalpha() and post_code[-2] not in not_allowed and
-              post_code[-3].isdigit())
-    return result
+    def _verify_third_position(self):
+        """
+        The only letters to appear in the third position are
+        ABCDEFGHJKPSTUW when the structure starts with A9A.
+        """
+        allowed = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'P', 'S',
+                   'T', 'U', 'W']
+        result = True
+        if self.post_code[2].isalpha():
+            result = self.post_code[2] in allowed
+        return result
 
-
-def _verify_first_position(post_code):
-    """
-    The first position is not a number
-    The letters QVX are not used in the first position
-    """
-    not_allowed = ('Q', 'V', 'X')
-    result = post_code[0].isalpha()
-    if post_code[0] in not_allowed:
-        result = False
-    return result
-
-
-def _verify_second_position(post_code):
-    """
-    The second position is a number or a letter
-    The letters IJZ are not used in the second position
-    """
-    not_allowed = ('I', 'J', 'Z')
-    result = post_code[1].isalpha() or post_code[1].isdigit()
-    if post_code[1] in not_allowed:
-        result = False
-    return result
-
-
-def _verify_third_position(post_code):
-    """
-    The only letters to appear in the third position are
-    ABCDEFGHJKPSTUW when the structure starts with A9A.
-    """
-    allowed = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'P', 'S', 'T',
-               'U', 'W']
-    result = True
-    if post_code[2].isalpha():
-        result = post_code[2] in allowed
-    return result
-
-
-def _verify_fourth_position(post_code):
-    """
-    The only letters to appear in the fourth position are
-    ABEHMNPRVWXY when the structure starts with AA9A.
-    """
-    allowed = ['A', 'B', 'E', 'H', 'M', 'N', 'P', 'R', 'V', 'W', 'X', 'Y']
-    result = True
-    if post_code[3].isalpha():
-        result = post_code[3] in allowed
-    return result
+    def _verify_fourth_position(self):
+        """
+        The only letters to appear in the fourth position are
+        ABEHMNPRVWXY when the structure starts with AA9A.
+        """
+        allowed = ['A', 'B', 'E', 'H', 'M', 'N', 'P', 'R', 'V', 'W', 'X', 'Y']
+        result = True
+        if self.post_code[3].isalpha():
+            result = self.post_code[3] in allowed
+        return result
--- tests.py
+++ tests.py
@@ -1,9 +1,6 @@
 import unittest
 
-from post_code import (_valid_size, _valid_space_position,
-                       _valid_last_3_positions, _verify_first_position,
-                       _verify_second_position, _verify_third_position,
-                       _verify_fourth_position, validate_post_code_for_uk)
+from post_code import PostCodeUK
 
 
 class TestPostCodeUK(unittest.TestCase):
@@ -11,157 +8,158 @@ class TestPostCodeUK(unittest.TestCase):
         self.post_code_size_6 = 'A9 9AA'
         self.post_code_size_7 = 'A9A 9AA'
         self.post_code_size_8 = 'AA9A 9AA'
+        self.post_code_uk = PostCodeUK()
 
     def test_verify_size_post_code_is_equal_6(self):
-        post_code = self.post_code_size_6
-        self.assertTrue(_valid_size(post_code))
+        self.post_code_uk.post_code = self.post_code_size_6
+        self.assertTrue(self.post_code_uk._valid_size())
 
     def test_verify_size_post_code_is_equal_7(self):
-        post_code = self.post_code_size_7
-        self.assertTrue(_valid_size(post_code))
+        self.post_code_uk.post_code = self.post_code_size_7
+        self.assertTrue(self.post_code_uk._valid_size())
 
     def test_verify_size_post_code_is_equal_8(self):
-        post_code = self.post_code_size_8
-        self.assertTrue(_valid_size(post_code))
+        self.post_code_uk.post_code = self.post_code_size_8
+        self.assertTrue(self.post_code_uk._valid_size())
 
     def test_verify_size_post_code_is_out_range(self):
-        post_code = 'AA9A9 OUT'
-        self.assertFalse(_valid_size(post_code))
+        self.post_code_uk.post_code = 'AA9A9 OUT'
+        self.assertFalse(self.post_code_uk._valid_size())
 
     def test_verify_space_position_is_2(self):
-        post_code = self.post_code_size_6
-        self.assertTrue(_valid_space_position(post_code))
+        self.post_code_uk.post_code = self.post_code_size_6
+        self.assertTrue(self.post_code_uk._valid_space_position())
 
     def test_verify_space_position_is_3(self):
-        post_code = self.post_code_size_7
-        self.assertTrue(_valid_space_position(post_code))
+        self.post_code_uk.post_code = self.post_code_size_7
+        self.assertTrue(self.post_code_uk._valid_space_position())
 
     def test_verify_space_position_is_4(self):
-        post_code = self.post_code_size_8
-        self.assertTrue(_valid_space_position(post_code))
+        self.post_code_uk.post_code = self.post_code_size_8
+        self.assertTrue(self.post_code_uk._valid_space_position())
 
     def test_verify_space_position_is_out_of_range(self):
-        post_code = 'AA9A9O U'
-        self.assertFalse(_valid_space_position(post_code))
+        self.post_code_uk.post_code = 'AA9A9O U'
+        self.assertFalse(self.post_code_uk._valid_space_position())
 
     def test_verify_last_3_positions_is_correct(self):
-        post_code = self.post_code_size_6
-        self.assertTrue(_valid_last_3_positions(post_code))
+        self.post_code_uk.post_code = self.post_code_size_6
+        self.assertTrue(self.post_code_uk._valid_last_3_positions())
 
     def test_verify_have_number_in_last_position(self):
-        post_code = 'A9 9A1'
-        self.assertFalse(_valid_last_3_positions(post_code))
+        self.post_code_uk.post_code = 'A9 9A1'
+        self.assertFalse(self.post_code_uk._valid_last_3_positions())
 
     def test_verify_not_allowed_letters_in_last_position(self):
         for letter in ['C', 'I', 'K', 'M', 'O', 'V']:
-            post_code = "AA9A 9A{0}".format(letter)
-            self.assertFalse(_valid_last_3_positions(post_code))
+            self.post_code_uk.post_code = "AA9A 9A{0}".format(letter)
+            self.assertFalse(self.post_code_uk._valid_last_3_positions())
 
     def test_verify_have_number_in_penultimate_position(self):
-        post_code = 'A9 91A'
-        self.assertFalse(_valid_last_3_positions(post_code))
+        self.post_code_uk.post_code = 'A9 91A'
+        self.assertFalse(self.post_code_uk._valid_last_3_positions())
 
     def test_verify_not_allowed_letters_in_penultimate_position(self):
         for letter in ['C', 'I', 'K', 'M', 'O', 'V']:
-            post_code = "AA9A 9{0}A".format(letter)
-            self.assertFalse(_valid_last_3_positions(post_code))
+            self.post_code_uk.post_code = "AA9A 9{0}A".format(letter)
+            self.assertFalse(self.post_code_uk._valid_last_3_positions())
 
     def test_verify_have_letter_in_antepenultimate_position(self):
-        post_code = 'A9 AAA'
-        self.assertFalse(_valid_last_3_positions(post_code))
+        self.post_code_uk.post_code = 'A9 AAA'
+        self.assertFalse(self.post_code_uk._valid_last_3_positions())
 
     def test_verify_first_position_is_letter(self):
-        post_code = self.post_code_size_6
-        self.assertTrue(_verify_first_position(post_code))
+        self.post_code_uk.post_code = self.post_code_size_6
+        self.assertTrue(self.post_code_uk._verify_first_position())
 
     def test_verify_first_position_is_not_letter(self):
-        post_code = '99 9AA'
-        self.assertFalse(_verify_first_position(post_code))
+        self.post_code_uk.post_code = '99 9AA'
+        self.assertFalse(self.post_code_uk._verify_first_position())
 
     def test_verify_first_position_is_not_Q(self):
-        post_code = 'Q9 9AA'
-        self.assertFalse(_verify_first_position(post_code))
+        self.post_code_uk.post_code = 'Q9 9AA'
+        self.assertFalse(self.post_code_uk._verify_first_position())
 
     def test_verify_first_position_is_not_V(self):
-        post_code = 'V9 9AA'
-        self.assertFalse(_verify_first_position(post_code))
+        self.post_code_uk.post_code = 'V9 9AA'
+        self.assertFalse(self.post_code_uk._verify_first_position())
 
     def test_verify_first_position_is_not_X(self):
-        post_code = 'X9 9AA'
-        self.assertFalse(_verify_first_position(post_code))
+        self.post_code_uk.post_code = 'X9 9AA'
+        self.assertFalse(self.post_code_uk._verify_first_position())
 
     def test_verify_second_position_is_not_letter(self):
-        post_code = self.post_code_size_6
-        self.assertTrue(_verify_second_position(post_code))
+        self.post_code_uk.post_code = self.post_code_size_6
+        self.assertTrue(self.post_code_uk._verify_second_position())
 
     def test_verify_second_position_is_letter(self):
-        post_code = self.post_code_size_7
-        self.assertTrue(_verify_second_position(post_code))
+        self.post_code_uk.post_code = self.post_code_size_7
+        self.assertTrue(self.post_code_uk._verify_second_position())
 
     def test_verify_second_position_is_I(self):
-        post_code = 'AI 9AA'
-        self.assertFalse(_verify_second_position(post_code))
+        self.post_code_uk.post_code = 'AI 9AA'
+        self.assertFalse(self.post_code_uk._verify_second_position())
 
     def test_verify_second_position_is_J(self):
-        post_code = 'AJ 9AA'
-        self.assertFalse(_verify_second_position(post_code))
+        self.post_code_uk.post_code = 'AJ 9AA'
+        self.assertFalse(self.post_code_uk._verify_second_position())
 
     def test_verify_second_position_is_Z(self):
-        post_code = 'AZ 9AA'
-        self.assertFalse(_verify_second_position(post_code))
+        self.post_code_uk.post_code = 'AZ 9AA'
+        self.assertFalse(self.post_code_uk._verify_second_position())
 
     def test_verify_third_position_is_space(self):
-        post_code = 'A9 9AA'
-        self.assertTrue(_verify_third_position(post_code))
+        self.post_code_uk.post_code = 'A9 9AA'
+        self.assertTrue(self.post_code_uk._verify_third_position())
 
     def test_verify_third_position_is_digit(self):
-        post_code = 'A99 9AA'
-        self.assertTrue(_verify_third_position(post_code))
+        self.post_code_uk.post_code = 'A99 9AA'
+        self.assertTrue(self.post_code_uk._verify_third_position())
 
     def test_verify_third_position_is_letter_and_incorrect(self):
-        post_code = 'A9Z 9AA'
-        self.assertFalse(_verify_third_position(post_code))
+        self.post_code_uk.post_code = 'A9Z 9AA'
+        self.assertFalse(self.post_code_uk._verify_third_position())
 
     def test_verify_third_position_is_letter_and_correct(self):
         allowed = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'P', 'S',
                    'T', 'U', 'W']
         for letter in allowed:
-            post_code = 'A9{0} 9AA'.format(letter)
-            self.assertTrue(_verify_third_position(post_code))
+            self.post_code_uk.post_code = 'A9{0} 9AA'.format(letter)
+            self.assertTrue(self.post_code_uk._verify_third_position())
 
     def test_verify_fourth_position_is_space(self):
-        post_code = 'A9A 9AA'
-        self.assertTrue(_verify_fourth_position(post_code))
+        self.post_code_uk.post_code = 'A9A 9AA'
+        self.assertTrue(self.post_code_uk._verify_fourth_position())
 
     def test_verify_fourth_position_is_number(self):
-        post_code = 'AA99 9AA'
-        self.assertTrue(_verify_fourth_position(post_code))
+        self.post_code_uk.post_code = 'AA99 9AA'
+        self.assertTrue(self.post_code_uk._verify_fourth_position())
 
     def test_verify_fourth_position_is_letter_and_incorrect(self):
-        post_code = 'AA9Z 9AA'
-        self.assertFalse(_verify_fourth_position(post_code))
+        self.post_code_uk.post_code = 'AA9Z 9AA'
+        self.assertFalse(self.post_code_uk._verify_fourth_position())
 
     def test_verify_fourth_position_is_letter_and_correct(self):
         allowed = ['A', 'B', 'E', 'H', 'M', 'N', 'P', 'R', 'V', 'W', 'X', 'Y']
         for letter in allowed:
-            post_code = 'AA9{0} 9AA'.format(letter)
-            self.assertTrue(_verify_fourth_position(post_code))
+            self.post_code_uk.post_code = 'AA9{0} 9AA'.format(letter)
+            self.assertTrue(self.post_code_uk._verify_fourth_position())
 
     def test_verify_post_code_with_size_6_is_correct(self):
         post_code = self.post_code_size_6
-        self.assertTrue(validate_post_code_for_uk(post_code))
+        self.assertTrue(self.post_code_uk.validate_post_code_for_uk(post_code))
 
     def test_verify_post_code_with_size_7_is_correct(self):
         post_code = self.post_code_size_7
-        self.assertTrue(validate_post_code_for_uk(post_code))
+        self.assertTrue(self.post_code_uk.validate_post_code_for_uk(post_code))
 
     def test_verify_post_code_with_size_8_is_correct(self):
         post_code = self.post_code_size_8
-        self.assertTrue(validate_post_code_for_uk(post_code))
+        self.assertTrue(self.post_code_uk.validate_post_code_for_uk(post_code))
 
     def test_verify_post_code_is_incorrect(self):
-        post_code = 'AAAA AAA'
-        self.assertFalse(validate_post_code_for_uk(post_code))
+        post_cod = 'AAAA AAA'
+        self.assertFalse(self.post_code_uk.validate_post_code_for_uk(post_cod))
 
 
 if __name__ == '__main__':
